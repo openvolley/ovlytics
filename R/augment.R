999ac2433e1a924ec6de690082a8b1129b850953
Ben Raymond <ben@theraymonds.org>
2023-03-10 20:01:40 +1100

    manual merge of dev v0.2.8

--- R/augment.R
+++ R/augment.R
@@ -4,25 +4,28 @@
 #' @param to_add character: columns to add
 #' * "receiving_team" adds the columns "receiving_team" (team name) and "receiving_team_id"
 #' * "winners" adds the columns "set_won_by", "set_won_by_id" (the name and ID of the team that won the current set), "match_won_by", "match_won_by_id" (the name and ID of the team that won the current match), "team_won_set" and "team_won_match" (did the team making the action in the current row win the set/match), and "home_sets_won" and "visiting_sets_won" (the number of sets won by the home and visiting teams)
-#' * "touch_summaries" adds a number of columns named "ts_*" that summarize a team touch (e.g. columns "ts_pass_quality", "ts_pass_evaluation_code" give the pass quality and pass evaluation code of the reception or dig associated with a given team touch)
+#' * "touch_summaries" adds a number of columns named "ts_*" that summarize a team touch (e.g. columns "ts_pass_quality", "ts_pass_evaluation_code" give the pass quality and pass evaluation code of the reception or dig associated with a given team touch). "touch_summaries" also adds a column named `freeball_over`, which disambiguates the action of putting a freeball over the net from the action of digging such a ball. Many scouts code both of these as a "Freeball". The `freeball_over` column will be `TRUE` if it was a freeball being put over the net, and `FALSE` otherwise (including freeball digs). Freeballs over and freeball digs will still both have "Freeball" in their `skill` column
 #' * "setters" adds the columns "home_setter_id", "visiting_setter_id" (the player IDs of the home and visiting setter on court), and "setter_id", "setter_position", and "setter_front_back" (the player ID and position of the setter from the team performing the current action)
 #' * "followed" adds the columns "followed_timeout", "followed_technical_timeout", and "followed_sub"
+#' * "player_role" add the column "player_role" which gives the role (outside, middle, opposite, setter) for the active player on each row of `x`. This assumes a standard rotation as specified by `rotation`. Note that `player_role` does NOT include libero, although this can be inferred from the `meta` component of a full datavolley object
 #' * "all" is a shortcut for all of the above
 #'
+#' @param rotation string: (only relevant when `to_add` includes "player_role") either "SHM" (assume a setter-hitter-middle rotation order, i.e. outside hitter is in position 2 when the setter is in 1), or "SMH" (setter-middle-hitter)
 #' @param use_existing logical: if `TRUE` and all of the columns associated with a given `to_add` choice are already present in `x`, they won't be re-generated
 #'
 #' @return `x` with the extra columns added
 #'
 #' @export
-ov_augment_plays <- function(x, to_add = c("receiving_team", "touch_summaries", "setters"), use_existing = TRUE) {
+ov_augment_plays <- function(x, to_add = c("receiving_team", "touch_summaries", "setters"), rotation = "SHM", use_existing = TRUE) {
     assert_that(is.character(to_add))
     assert_that(is.flag(use_existing), !is.na(use_existing))
     to_add <- tolower(to_add)
-    known_to_add <- c("receiving_team", "touch_summaries", "setters", "followed", "winners") ## the allowed values
+    known_to_add <- c("receiving_team", "touch_summaries", "setters", "followed", "winners", "player_role") ## the allowed values
     if ("all" %in% to_add) to_add <- known_to_add
     if (!all(to_add %in% known_to_add)) {
         warning("unrecognized 'to_add' values, ignoring: ", paste0(setdiff(to_add, known_to_add)))
     }
+    if ("player_role" %in% to_add) rotation <- match.arg(rotation, c("SHM", "SMH"))
 
     if ("receiving_team" %in% to_add && (!all(c("receiving_team", "receiving_team_id") %in% names(x)) || !use_existing)) {
         x <- dplyr::mutate(x, receiving_team = case_when(.data$serving_team %eq% .data$home_team ~ .data$visiting_team,
@@ -31,7 +34,78 @@ ov_augment_plays <- function(x, to_add = c("receiving_team", "touch_summaries", 
                                                       .data$serving_team %eq% .data$visiting_team ~ .data$home_team_id))
     }
 
-    if ("touch_summaries" %in% to_add && (!all(c("ts_pass_quality", "ts_set_error", "ts_pass_evaluation_code") %in% names(x)) || !use_existing)) {
+    if ("player_role" %in% to_add && (!"player_role" %in% names(x) || !use_existing)) {
+        x <- mutate(x, player_current_position = case_when(.data$player_id == .data$home_player_id1 ~ 1,
+                                                           .data$player_id == .data$visiting_player_id1 ~ 1,
+                                                           .data$player_id == .data$home_player_id2 ~ 2,
+                                                           .data$player_id == .data$visiting_player_id2 ~ 2,
+                                                           .data$player_id == .data$home_player_id3 ~ 3,
+                                                           .data$player_id == .data$visiting_player_id3 ~ 3,
+                                                           .data$player_id == .data$home_player_id4 ~ 4,
+                                                           .data$player_id == .data$visiting_player_id4 ~ 4,
+                                                           .data$player_id == .data$home_player_id5 ~ 5,
+                                                           .data$player_id == .data$visiting_player_id5 ~ 5,
+                                                           .data$player_id == .data$home_player_id6 ~ 6,
+                                                           .data$player_id == .data$visiting_player_id6 ~ 6),
+                    temp_setter_position = case_when(.data$team == .data$home_team ~ .data$home_setter_position,
+                                                     .data$team == .data$visiting_team ~ .data$visiting_setter_position))
+        if (rotation == "SHM") {
+            x <- dplyr::mutate(x,
+                               player_role = case_when(.data$temp_setter_position == 1 & .data$player_current_position %in% c(2, 5) ~ "outside",
+                                                       .data$temp_setter_position == 1 & .data$player_current_position %in% c(3, 6) ~ "middle",
+                                                       .data$temp_setter_position == 1 & .data$player_current_position == 4 ~ "opposite",
+                                                       .data$temp_setter_position == 1 & .data$player_current_position == 1 ~ "setter",
+                                                       .data$temp_setter_position == 2 & .data$player_current_position %in% c(3, 6) ~ "outside",
+                                                       .data$temp_setter_position == 2 & .data$player_current_position %in% c(4, 1) ~ "middle",
+                                                       .data$temp_setter_position == 2 & .data$player_current_position == 5 ~ "opposite",
+                                                       .data$temp_setter_position == 2 & .data$player_current_position == 2 ~ "setter",
+                                                       .data$temp_setter_position == 3 & .data$player_current_position %in% c(4, 1) ~ "outside",
+                                                       .data$temp_setter_position == 3 & .data$player_current_position %in% c(5, 2) ~ "middle",
+                                                       .data$temp_setter_position == 3 & .data$player_current_position == 6 ~ "opposite",
+                                                       .data$temp_setter_position == 3 & .data$player_current_position == 3 ~ "setter",
+                                                       .data$temp_setter_position == 4 & .data$player_current_position %in% c(5, 2) ~ "outside",
+                                                       .data$temp_setter_position == 4 & .data$player_current_position %in% c(6, 3) ~ "middle",
+                                                       .data$temp_setter_position == 4 & .data$player_current_position == 1 ~ "opposite",
+                                                       .data$temp_setter_position == 4 & .data$player_current_position == 4 ~ "setter",
+                                                       .data$temp_setter_position == 5 & .data$player_current_position %in% c(6, 3) ~ "outside",
+                                                       .data$temp_setter_position == 5 & .data$player_current_position %in% c(1, 4) ~ "middle",
+                                                       .data$temp_setter_position == 5 & .data$player_current_position == 2 ~ "opposite",
+                                                       .data$temp_setter_position == 5 & .data$player_current_position == 5 ~ "setter",
+                                                       .data$temp_setter_position == 6 & .data$player_current_position %in% c(1, 4) ~ "outside",
+                                                       .data$temp_setter_position == 6 & .data$player_current_position %in% c(2, 5) ~ "middle",
+                                                       .data$temp_setter_position == 6 & .data$player_current_position == 3 ~ "opposite",
+                                                       .data$temp_setter_position == 6 & .data$player_current_position == 6 ~ "setter"))
+        } else {
+            x <- dplyr::mutate(x,
+                               player_role = case_when(.data$temp_setter_position == 1 & .data$player_current_position %in% c(2, 5) ~ "middle",
+                                                       .data$temp_setter_position == 1 & .data$player_current_position %in% c(3, 6) ~ "outside",
+                                                       .data$temp_setter_position == 1 & .data$player_current_position == 4 ~ "opposite",
+                                                       .data$temp_setter_position == 1 & .data$player_current_position == 1 ~ "setter",
+                                                       .data$temp_setter_position == 2 & .data$player_current_position %in% c(3, 6) ~ "middle",
+                                                       .data$temp_setter_position == 2 & .data$player_current_position %in% c(4, 1) ~ "outside",
+                                                       .data$temp_setter_position == 2 & .data$player_current_position == 5 ~ "opposite",
+                                                       .data$temp_setter_position == 2 & .data$player_current_position == 2 ~ "setter",
+                                                       .data$temp_setter_position == 3 & .data$player_current_position %in% c(4, 1) ~ "middle",
+                                                       .data$temp_setter_position == 3 & .data$player_current_position %in% c(5, 2) ~ "outside",
+                                                       .data$temp_setter_position == 3 & .data$player_current_position == 6 ~ "opposite",
+                                                       .data$temp_setter_position == 3 & .data$player_current_position == 3 ~ "setter",
+                                                       .data$temp_setter_position == 4 & .data$player_current_position %in% c(5, 2) ~ "middle",
+                                                       .data$temp_setter_position == 4 & .data$player_current_position %in% c(6, 3) ~ "outside",
+                                                       .data$temp_setter_position == 4 & .data$player_current_position == 1 ~ "opposite",
+                                                       .data$temp_setter_position == 4 & .data$player_current_position == 4 ~ "setter",
+                                                       .data$temp_setter_position == 5 & .data$player_current_position %in% c(6, 3) ~ "middle",
+                                                       .data$temp_setter_position == 5 & .data$player_current_position %in% c(1, 4) ~ "outside",
+                                                       .data$temp_setter_position == 5 & .data$player_current_position == 2 ~ "opposite",
+                                                       .data$temp_setter_position == 5 & .data$player_current_position == 5 ~ "setter",
+                                                       .data$temp_setter_position == 6 & .data$player_current_position %in% c(1, 4) ~ "middle",
+                                                       .data$temp_setter_position == 6 & .data$player_current_position %in% c(2, 5) ~ "outside",
+                                                       .data$temp_setter_position == 6 & .data$player_current_position == 3 ~ "opposite",
+                                                       .data$temp_setter_position == 6 & .data$player_current_position == 6 ~ "setter"))
+        }
+        x <- dplyr::select(x, -"temp_setter_position", -"player_current_position")
+    }
+
+    if ("touch_summaries" %in% to_add && (!all(c("ts_pass_quality", "ts_set_error", "ts_pass_evaluation_code", "freeball_over") %in% names(x)) || !use_existing)) {
         x <- x[, setdiff(names(x), c("ts_pass_quality", "ts_set_error", "ts_pass_evaluation_code"))]
         if (!"freeball_over" %in% names(x)) {
             ## "Freeball" skill can be used both for sending a freeball to the opposition as well as receiving one, so disambiguate these usages
